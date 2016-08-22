e5658035c68721d74de913441f06de2b4df33474
Ramil Safin <RamilSafNab1996@gmail.com>
2016-08-22 20:20:46 +0300

    Add web support
    Add WebInitializer, WebConfig, ftl index file

---
+++ src/main/java/ru/kpfu/itis/config/WebConfig.java
@@ -0,0 +1,44 @@
+package ru.kpfu.itis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = {"ru.kpfu.itis.controller"})
+
+@EnableWebMvc //Enable web mvc configuration by spring
+public class WebConfig extends WebMvcConfigurerAdapter { //extending it to customize web mvc configuration
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/freemarker/");
+        configurer.setDefaultEncoding("UTF-8");
+
+        return configurer;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html; charset=UTF-8");
+        return resolver;
+    }
+
+}
---
+++ src/main/java/ru/kpfu/itis/config/WebInitializer.java
@@ -0,0 +1,22 @@
+package ru.kpfu.itis.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//instead of web.xml
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
